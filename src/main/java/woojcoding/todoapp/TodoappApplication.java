8a120a065795e9430162255e22b25a3e54346ef1
woojcoding <110895459+woojcoding@users.noreply.github.com>
2022-12-12 17:32:25 +0900

    JAR → WAR 포맷 변경
    id 'war' 추가
    
    SpringBootServletInitializer를 상속
    config를 Override

--- src/main/java/woojcoding/todoapp/TodoappApplication.java
+++ src/main/java/woojcoding/todoapp/TodoappApplication.java
@@ -2,12 +2,18 @@ package woojcoding.todoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TodoappApplication {
+public class TodoappApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoappApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(TodoappApplication.class);
+	}
 }
